356a968906813b4931bbd3fc29aa7942b3b2a52c
Bridget Wiffler <bmwiff18@g.holycross.edu>
2020-11-30 14:08:35 -0500

    Add app class

---
+++ Pinch.cpp
@@ -0,0 +1,13 @@
+//
+//  Pinch.cpp
+//  
+//
+//  Created by Bridget Wiffler on 11/30/20.
+//
+
+#include "Pinch.hpp"
+
+Pinch::Pinch(const std::string& config_) : _config{config_}
+{
+  std::cout << "Created Pinch" << std::endl;
+}
---
+++ Pinch.hpp
@@ -0,0 +1,21 @@
+//
+//  Pinch.hpp
+//  
+//
+//  Created by Bridget Wiffler on 11/30/20.
+//
+
+#ifndef Pinch_hpp
+#define Pinch_hpp
+
+#include <stdio.h>
+
+class Pinch
+{
+public:
+  Pinch(const std::string& config_);
+  
+private:
+  const std::string _config;
+};
+#endif /* Pinch_hpp */
